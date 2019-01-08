a9a7dc3b65f2c77f1a8f14c2f7d27be2c006b8f8
rbrown3 <rosannabrown@outlook.com>
2019-01-08 17:09:50 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
 
+class CashRegister
+  attr_accessor :total ,:discount
+
+
+  def initialize(total=0, discount=20)
+    @total=total
+    @discount=discount
+  end
+
+  def add_item(title, num, quantity=1)
+  new_total =  num * quantity
+
+    @total += new_total
+  end
+
+  def apply_discount
+  if discount
+      total_with_discount = @total * discount / 100.0
+  end
+    return @total - total_with_discount
+  end
+  def items
+   items_array = []
+ end
+def void_last_transaction
+
+end
+end

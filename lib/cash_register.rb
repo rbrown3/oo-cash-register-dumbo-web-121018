af1dffccaa1dadac544f69587005caa4cb3b16a7
rbrown3 <rosannabrown@outlook.com>
2019-01-08 20:47:24 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,29 +1,62 @@
-
 class CashRegister
-  attr_accessor :total ,:discount
 
+  attr_accessor :total, :discount, :price
 
-  def initialize(total=0, discount=20)
-    @total=total
-    @discount=discount
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
   end
+  def add_item(item, price, quantity = 1)
+      @price = price
+      @total += price * quantity
+      if quantity > 1
+        counter = 0
+        while counter < quantity
+          @items << item
+          counter += 1
+        end
+      else
+        @items << item
+      end
+    end
+  #not needed using attr_accessor
+  #def total
+   # return @total
+  #  puts "The Current Total is $#{@total}."
+  #end
 
-  def add_item(title, num, quantity=1)
-  new_total =  num * quantity
-
-    @total += new_total
-  end
+  # #def add_item(item, price, quantity = 1)
+  #   @sub_total = price * quantity
+  #
+  #   self.total += @sub_total
+  #     if quantity > 1
+  #       while quantity > 0
+  #        @items << item
+  #        quantity -= 1
+  #       end
+  #     elsif
+  #       @items << item
+  #   end
+  # #end
 
   def apply_discount
-  if discount
-      total_with_discount = @total * discount / 100.0
-  end
-    return @total - total_with_discount
-  end
-  def items
-   items_array = []
- end
-def void_last_transaction
 
-end
-end
+    if @discount > 0
+      @discount_amount = (self.total * @discount)/100
+      self.total -= @discount_amount
+      return "After the discount, the total comes to $#{@total}."
+    else
+      return "There is no discount to apply."
+    end
+  end
+
+  def items
+     @items
+  end
+
+  def void_last_transaction
+     @total -= @price
+  end
+
+  end#end class
